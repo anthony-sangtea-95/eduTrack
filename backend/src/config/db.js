0d5981ae84d9c5ae31e58972c1f04e14e790d17f
heinthetsoe <softguide.heinthetsoe@gmail.com>
2025-11-11 17:24:58 +0630

    modified-20251111

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?appName=AtlasCluster`;
+        const conn = await mongoose.connect(uri);
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`❌ Error: ${error.message}`);
